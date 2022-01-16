79dafe732d2c1657dc2b6ac117337624e2154393
Aden-Adar <77550086+Aden-Adar@users.noreply.github.com>
2022-01-15 20:38:38 -0700

    models & db setup
    - Created two models (clothes & outfits)
    
    - Initialized the Hive db

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui/screens/home_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() => runApp(MyApp());
+void main() async {
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  runApp(MyApp());
+
+  final clothingBox = await Hive.openBox('clothing');
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
